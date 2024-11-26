d736ea6f22a49332a06d851d385a99690de6afa4
Ankita <ankita1908sharma@gmail.com>
2024-11-27 00:03:38 +0530

    03_conversionOperations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -2,45 +2,45 @@
 
 let score = "50asd"
 
-console.log(typeof score);
-console.log(typeof (score));
+//console.log(typeof score);
+//console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 // score is null
 
 let score1 = null
 
-console.log(typeof score1);
-console.log(typeof (score1));
+//console.log(typeof score1);
+//console.log(typeof (score1));
 
 let valueInNumber1 = Number(score1)
-console.log(typeof valueInNumber1);
-console.log(valueInNumber1);
+//console.log(typeof valueInNumber1);
+//console.log(valueInNumber1);
 
 // score is undefined
 
 let score2 = undefined
 
-console.log(typeof score2);
-console.log(typeof (score2));
+//console.log(typeof score2);
+//console.log(typeof (score2));
 
 let valueInNumber2 = Number(score2)
-console.log(typeof valueInNumber2);
-console.log(valueInNumber2);
+//console.log(typeof valueInNumber2);
+//console.log(valueInNumber2);
 
 //score is a boolean value
 
 let score3 = true
 
-console.log(typeof score3);
-console.log(typeof (score3));
+//console.log(typeof score3);
+//console.log(typeof (score3));
 
 let valueInNumber3 = Number(score3)
-console.log(typeof valueInNumber3);
-console.log(valueInNumber3);
+//console.log(typeof valueInNumber3);
+//console.log(valueInNumber3);
 
 // "50" => 50
 // "50asd" => NaN
@@ -49,18 +49,18 @@ console.log(valueInNumber3);
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 
 let isLoggedIn1 = ""
 
 let booleanIsLoggedIn1 = Boolean(isLoggedIn1)
-console.log(booleanIsLoggedIn1);
+// console.log(booleanIsLoggedIn1);
 
 let isLoggedIn2 = "Ankita"
 
 let booleanIsLoggedIn2 = Boolean(isLoggedIn2)
-console.log(booleanIsLoggedIn2);
+// console.log(booleanIsLoggedIn2);
 
 // 1 => true; 0 => false
 // "" => false
@@ -68,7 +68,46 @@ console.log(booleanIsLoggedIn2);
 
 let someNumber = 50
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
+
+
+// ********************** Operations ************************* //
+
+let value = 3
+let negvalue = -value
+//console.log(negvalue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 3);
+// console.log(2 ** 4);
+// console.log(2 / 2);
+// console.log(111 % 33 );
+
+let str = "hello"
+let str2 = "vannkam"
+
+let str3 = str + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(1+(2*(3+4)));
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gamecounter = 100;
+gamecounter++;
+console.log(gamecounter);
