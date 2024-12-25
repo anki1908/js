1f97da631ab5bc2e6c171ec9e9dbbc44f15c827e
Ankita <ankita1908sharma@gmail.com>
2024-12-25 20:57:11 +0530

    data-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,31 @@
+// primitive
+// 7 types: string, number, boolean, null, undefined, symbol and BigInt
+
+//const score = 100
+//const scoreValue = 100.3
+//const isLoggedIn = false
+//const outSideTemp = null
+//let userEmail;
+
+//const id = Symbol('123')
+//const anotherId = Symbol('123')
+
+//console.log(id == anotherId);
+
+//const bigNumber = 78456621444425364n
+
+// Reference (non primitive)
+// Array, objects, functions
+
+const heros = ["shaktiman", "udibaba"]
+let myObj = {
+    name: "jj",
+    age: 22,
+}
+
+const myFunction = function () {
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
