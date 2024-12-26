a7c01ddae047163ec8e66007554c1b1230eb861c
Ankita <ankita1908sharma@gmail.com>
2024-12-26 22:53:10 +0530

    unmus_and_maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,35 @@
+const score = 200
+//console.log(score);
+
+
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString());
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(4));
+
+const otherNumber = 25.14526
+//console.log(otherNumber.toPrecision(5));
+
+const numbers = 100000
+//console.log(numbers.toLocaleString('en-IN'));
+
+// ++++++++++++++++++++ Maths +++++++++++++++++++++++
+
+//console.log(Math);
+//console.log(Math.abs(-4));
+//console.log(Math.round(4.9));
+//console.log(Math.ceil(4.1));
+///console.log(Math.floor(4.1));
+//console.log(Math.min(4,3,5,6,8,9,4));
+//console.log(Math.max(4, 3, 5, 6, 8, 9, 4));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
