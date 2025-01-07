f1c211976c02d9c32575bc821443c4c63ca9c042
Ankita <ankita1908sharma@gmail.com>
2025-01-07 22:16:36 +0530

    changes

--- 03_basics/02_scops.js
+++ 03_basics/02_scops.js
@@ -7,10 +7,51 @@ let a = 300
 if(true){
     let a = 10
     const b = 20
-    console.log("Inner: ", a);
+    //console.log("Inner: ", a);
     
 }
 
-console.log(a);
+//console.log(a);
 //console.log(b);
 //console.log(c);
+
+
+function one() {
+    const username = "Ankita"
+
+    function two() {
+        const website = "youtube"
+        console.log(username);
+    }
+    //console.log(website);
+
+    two()
+    
+}
+
+//one()
+
+if (true) {
+    const username = "cheenu"
+    if (username === "cheenu") {
+        const website = " youtube"
+        //console.log(username + website);
+        
+    }
+    //console.log(websit );
+    
+}
+//console.log(username);
+
+
+//++++++++++++++++++++++++++ interesting ++++++++++++++++++++++++++++
+console.log(addOne(5));
+function addOne(num) {
+    return num +1
+}
+
+
+addTwo(5)
+const addTwo = function (num) {
+    return num + 2
+}
