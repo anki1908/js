2349d13c506f892596e81a1d72fbcd3cd7e015d7
Ankita <ankita1908sharma@gmail.com>
2025-03-11 23:32:09 +0530

    control_flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,19 @@
+// if
+
+//const isUserLoggedIn = true
+//const temperature = 41
+//
+//if ( temperature > 50 ) {
+//    console.log("greater than 50");
+//} else {
+//    console.log("less than 50");
+//}
+
+//const score = 500
+
+//if (score > 100) {
+//    const power = "fly"
+//    console.log(`User power : ${power}`);
+//}
+
+// if the log power line was written outside the if loop then the powee will be udefined but if the const power was var power than that value of power can be used outside the loop, that is why let is mostky used to declare a variable
