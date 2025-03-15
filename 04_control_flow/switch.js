dadf99555f30326bcfd12138c6ff8990e0b79eaa
Ankita <ankita1908sharma@gmail.com>
2025-03-15 23:44:28 +0530

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,32 @@
+//switch (key) {
+//   case value:
+//        
+//        break;
+//
+//    default:
+        break;
+//}
+
+const month = 4
+switch (month) {
+
+    case 1:
+        console.log("January"); 
+        break;
+    
+    case 2:
+        console.log("feb"); 
+        break;
+    
+    case 3:
+        console.log("march"); 
+        break;
+    
+    case 4:
+        console.log("faprileb"); 
+        break;
+
+    default:
+        console.log("Value did not matched");
+        break;
+}
