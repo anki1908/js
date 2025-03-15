d40818eee17ec6801c4c2c08c12aa0494eb56a61
Ankita <ankita1908sharma@gmail.com>
2025-03-15 23:56:05 +0530

    changes

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,17 @@
+const userEmail = "a@ment.co"
+
+if (userEmail) {
+    console.log("Got user mail"); 
+} else {
+    console.log("Don't have user Mail");  
+}
+
+// falsy values
+//
+//false, 0, -0, BigInt 0n, "",  null, undefined, NaN
+//
+
+
+//truthy values
+//
+//"0", 'fasle', " ", [], {}, function(){}, 
