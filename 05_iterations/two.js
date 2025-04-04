fe722bf48ca0a17e9f41adb6c3f8941e199a5856
Ankita <ankita1908sharma@gmail.com>
2025-04-04 23:03:56 +0530

    chnges

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+
+//let j = 0
+//while ( j  <= 10 ) {
+//    console.log(`Value of J is ${j}`);
+//    j = j + 2
+//}
+
+//let myArray = ['ab', 'bc', 'cd']
+//let arr = 0
+//while (arr < myArray.length) {
+//    console.log(`Value of myArray is ${myArray[arr]}`);
+//    arr = arr + 1
+//}
+
+let score = 1
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
